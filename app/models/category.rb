5c725b2b1c513f5f93c16eb7a9c0c0b8ccb92134
Jesus Francisco Rodriguez Perez <tolesco@gmail.com>
2019-08-25 20:21:27 -0500

    Improve views for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :money_transactions
   enum associated_transaction_type: [:deposit, :withdrawal]
+  after_commit :uncategorized_child_transactions, on: :destroy
+
+  def uncategorized_child_transactions
+    # Search for transactions and uncategorized
+  end
 end
